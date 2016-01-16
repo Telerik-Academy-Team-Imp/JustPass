5cd8ea069b5c45a079f23e5bacda6519aef213f7
zdzdz <zdzdz.link@yahoo.com>
2016-01-16 23:35:01 +0200

    Some fixes for the pinch zoom.

--- app/src/main/java/com/example/tectonik/justpass/ScaleImageActivity.java
+++ app/src/main/java/com/example/tectonik/justpass/ScaleImageActivity.java
@@ -1,15 +1,26 @@
 package com.example.tectonik.justpass;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+import com.example.tectonik.justpass.customViews.ScaleImageView;
+
 public class ScaleImageActivity extends AppCompatActivity {
+    ScaleImageView pictureViewer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scale_image);
+
+        pictureViewer =  (ScaleImageView) findViewById(R.id.picture_viewer);
+        //Bundle extras = getIntent().getExtras();
+        //Bitmap bmp = extras.getParcelable("imagebitmap");
+        pictureViewer.setImageResource(R.drawable.profile_pic);
 
 //        Bundle extras = getIntent().getExtras();
 //        int navPage = extras.getInt(Constants.PAGE);
--- app/src/main/java/com/example/tectonik/justpass/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/tectonik/justpass/fragments/ProfileFragment.java
@@ -1,6 +1,10 @@
 package com.example.tectonik.justpass.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,22 +20,32 @@ import com.example.tectonik.justpass.helpers.Constants;
 public class ProfileFragment extends Fragment {
     ImageView profilePicture;
     ScaleImageView pictureViewer;
+    Bitmap pic;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
         profilePicture = (ImageView) rootView.findViewById(R.id.profile_picture);
-        pictureViewer =  (ScaleImageView)rootView.findViewById(R.id.picture_viewer);
-        profilePicture.setTag(R.drawable.profile_pic);
+        pictureViewer =  (ScaleImageView) rootView.findViewById(R.id.picture_viewer);
+
+
         profilePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                profilePicture.buildDrawingCache();
+//                pic = profilePicture.getDrawingCache();
+//                Drawable t = profilePicture.getDrawable().getCurrent();
+
                 Intent intent = new Intent(getActivity(), ScaleImageActivity.class);
+               // Bundle extras = new Bundle();
+
                 intent.putExtra(Constants.PAGE, 2);
+               // intent.putExtra("bmp", t);
+                //int id = getResources().getIdentifier();
+                //profilePicture.setImageResource(t);
+
                 startActivity(intent);
-                
-               // pictureViewer.setImageResource((int)profilePicture.getTag());
             }
         });
         return rootView;
