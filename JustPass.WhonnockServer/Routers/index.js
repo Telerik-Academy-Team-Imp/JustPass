7639be0a276068b35a81f24104fa515946596957
Martin Ali <marto7395@gmail.com>
2016-01-17 21:27:27 +0200

    Sync conflicts

--- JustPass.WhonnockServer/Routers/index.js
+++ JustPass.WhonnockServer/Routers/index.js
@@ -7,8 +7,13 @@
 	module.exports = function(app) {
 		fileSystem
 			.readdirSync(constants.ROUTERS_LOCATION)
+<<<<<<< HEAD
+			// .filter(file => file == 'advices-router.js')
+			.filter(file => file != 'index.js')
+=======
 			.filter(file => file == 'advices-router.js')
 			// .filter(file => file != 'index.js')
+>>>>>>> 6e5be9e9605548bca625567836bafcc55d7542d2
 			.forEach(file => require(`./${file}`)(app));
 
 		// // require('./advices-router.js')(app);
